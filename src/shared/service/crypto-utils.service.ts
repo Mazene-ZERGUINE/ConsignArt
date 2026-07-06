f8aad2be17c73e32d542b020acda7ba4ac31f77a
Mazene <mmazenezerguine@gmail.com>
2026-07-06 16:32:18 +0200

    feat(auth): password hashing service

---
+++ src/shared/service/crypto-utils.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class CryptoUtilsService {
+  private readonly DEFAULT_SALT_ROUNDS = 10;
+
+  constructor(private configService: ConfigService) {}
+
+  public async hashPasswordWithBcrypt(plainText: string): Promise<string> {
+    const saltRounds = this.configService.get<number>('SALT_ROUNDS', this.DEFAULT_SALT_ROUNDS);
+    return await bcrypt.hash(plainText, saltRounds);
+  }
+}
